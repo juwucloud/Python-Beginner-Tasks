25788b86fac4c92e05308667ca7be6bc1a4fd0b9
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-10-18 15:07:42 +0200

    [ADD] Added query loop to avoid ValueErrors. Some minor optical changes and Timer ends on 'Times up' instead of 0

--- 10-Countdown-Timer.py
+++ 10-Countdown-Timer.py
@@ -1,12 +1,17 @@
 import time
 
-sec = int(input("How long do you want to wait? (seconds) \n\n"))
-print("")
-print("")
 
+while True:
+    try:
+        sec = int(input("How long do you want to wait? (seconds) "))
+        break
+    except ValueError:
+        print("Please enter a valid number")
+        
+print("")
 
                 
-for i in range(sec, -1, -1):
+for i in range(sec, 0, -1):
     print (i)
     time.sleep(1)
 print ("Time's up!")
