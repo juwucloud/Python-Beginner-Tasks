2e38bfd399f52210b7cc8a7fb0603e7096bf1061
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-10-18 15:02:27 +0200

    [ADD] Added countdown with time module.

--- 10-Countdown-Timer.py
+++ 10-Countdown-Timer.py
@@ -1,3 +1,13 @@
 import time
 
+sec = int(input("How long do you want to wait? (seconds) \n\n"))
+print("")
+print("")
+
+
+                
+for i in range(sec, -1, -1):
+    print (i)
+    time.sleep(1)
+print ("Time's up!")
 
