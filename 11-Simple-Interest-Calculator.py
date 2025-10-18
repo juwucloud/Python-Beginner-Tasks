5839a13dfa4a0dcc19360e681e09199eeffac8ba
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-10-18 15:37:44 +0200

    [ADD] Safety for invalid values

--- 11-Simple-Interest-Calculator.py
+++ 11-Simple-Interest-Calculator.py
@@ -1,9 +1,28 @@
 print("Welcome to the Interest Calculator!")
 print("Tell us your Principal, Rate and the Time for your Interest")
 
-principal = float(input("Principal (€): "))
-rate = float(input("Rate: "))
-time = int(input("Time (in years): "))
+
+while True:
+    try:
+        principal = float(input("Principal (€): ")).replace(",", "."))
+        break
+    except ValueError:
+        print("Please enter a valid number")
+
+while True:
+    try:
+        rate = float(input("Rate: ")).replace(",", "."))
+        break
+    except ValueError:
+        print("Please enter a valid number")
+
+while True:
+    try:
+        time = int(input("Time (in years): ")).replace(",", "."))
+        break
+    except ValueError:
+        print("Please enter a valid number")
+
 
 interest = (principal * rate * time) / 100
 
