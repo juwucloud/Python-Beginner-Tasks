3440c84c27cfb35b30f35badf95dfa3d44cee69f
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-10-18 13:34:41 +0200

    [FIX]Palicheck only checked the first characters, now it cycles completly except the characters differ. also made it repeatable. Added .lower to avoid lowercase - uppercase problems

--- 08-PalindromeChecker.py
+++ 08-PalindromeChecker.py
@@ -1,16 +1,27 @@
-word = input ("Is your word a palindrome? Type your word\n")
-
 def palicheck(a):
     rev = a[::-1]
     for letter in range(len(a)):
-        if a == rev:
-            comparison = True
-        else:
-            comparison = False
-    return comparison
-    
-        
-if palicheck(word) == True:
-    print (f"The word {word} is a palindrome!")
-else:
-    print (f"The word {word} is NOT a palindrome!")
+        if a[letter] != rev[letter]:
+            return False
+    return True
+
+# Main
+def main():
+    word = input ("Is your word a palindrome? Type your word\n")
+    if palicheck(word.lower()) == True:
+        print (f"The word {word} is a palindrome!")
+    else:
+        print (f"The word {word} is NOT a palindrome!")
+
+
+main()
+
+while True:
+    again = input ("Test another word? (y/n) ".lower())
+    if again == "y" or again =="yes":
+        main()
+    elif again == "n" or again == "no":
+        print ("Thank you for using PalindromeChecker")
+        break
+    else:
+        print ("Invalid Character, please type y or n")
